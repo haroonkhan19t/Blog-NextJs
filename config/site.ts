239305b94ceb8e9f190b73896594987ec1456ecc
haroonkhan4149 <haroon.khan4149@gmail.com>
2024-07-09 12:51:42 +0500

    site Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Home",
+    url: "https://example.com",
+    description: "Nextjs 14 blog using velite, tailwind and shadcn",
+    author: "HaroonKhan",
+    links: {
+      github: "https://github.com/haroonkhan19t",
+      personalSite: "https://jamesshopland.com",
+      linkedin:"https://www.linkedin.com/in/haroon-khan-0570b7234/"
+    },
+  };
+  
+  export type SiteConfig = typeof siteConfig;
